3e61afea108a05897210b0fe314a454bd428ea9c
Jason McCallister <themccallister@gmail.com>
2019-07-10 20:07:25 -0400

    ga

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -29,7 +29,7 @@ module.exports = {
         [
             '@vuepress/google-analytics',
             {
-                'ga': ''
+                'ga': 'UA-143687854-1'
             }
         ]
     ]
